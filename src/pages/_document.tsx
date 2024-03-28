2108334877854e20bd9a39b39737046b7e098228
Blake Ferm <blakeferm@gmail.com>
2024-03-28 13:23:27 -0400

    update favicon in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,8 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
-          ></link>
+          />
+          <link rel="icon" type="image/png" href="/favicon.png" />
         </Head>
         <body>
           <Main />
